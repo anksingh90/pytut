3dd1343169d090a983258dea3e6fff8e164aadf1
v41nw7jl <167311253+v41nw7jl@users.noreply.github.com>
2025-01-09 09:34:09 +0000

    changes

--- oops/6.py
+++ oops/6.py
@@ -1,4 +1,6 @@
-# Create an Electric Car Class that inherits from Class Car and have additional attribute - battery size
+# The error you're seeing is because you're printing the object directly,
+# which by default shows the object's memory address
+# To print object value, you need to define __str__ method in your Electric_car class.
 
 class Car:
     def __init__(self, u_brand, u_model):
@@ -13,7 +15,9 @@ class Electric_car(Car):
     def __init__(self, brand, model, battery_size):
         super().__init__(brand,model)
         self.battery_size = battery_size
+    
+    def __str__(self):
+        return f"{self.full_name()}, Battery Size: {self.battery_size}"
 
 mm = Electric_car('Mahindra', 'Be 6', '80 kWh')
-print(mm.model, mm.full_name())
-print(mm) # printing object directly
+print(mm)
