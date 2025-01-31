4fd956b0cb5dde43f3ac68c2734a237ca0e7e639
anksingh90 <ankitsingh90@duck.com>
2025-01-31 13:21:21 +0000

    changes

--- file_handle/f6.py
+++ file_handle/f6.py
@@ -1,8 +1,12 @@
+import random as rm
+
 count = 0 
 with open('ex.txt','r') as file:
-    read_mode = file.read().split()
-    print(len(read_mode))
-    for i in read_mode:
-       if i.isspace():
-            count+=1
-print(count)
+    read_mode = file.readlines()
+    print('read_mode',read_mode)
+    file.seek(0)
+    read1 = file.read()
+    file.seek(0)
+    print('read1',read1)
+    read2 = file.readline()
+    print('read2',read2)
---
+++ file_handle/f7.py
@@ -0,0 +1,2 @@
+with open ('ex.txt', 'w') as file:
+    write_mode = file.writelines(['this line of text is being added\n','hellohellohelllo\n'])
