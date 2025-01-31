2f5d704dea0ef8d79eead0e3c45f1e3812f1cb69
anksingh90 <ankitsingh90@duck.com>
2025-01-31 05:05:27 +0000

    change

---
+++ file_handle/f5.py
@@ -0,0 +1,12 @@
+import os
+
+file_path = os.path.join("data", "ex.txt")      # Joins path components correctly for different operating systems.
+absolute_path = os.path.abspath(file_path)      # Gets the absolute path
+print(f"absolute path: {absolute_path}")
+if os.path.exists(file_path):
+    print(f"File exists: {file_path}")
+
+with open("data.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
