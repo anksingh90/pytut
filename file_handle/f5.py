86fce71a6eb228e00ada7cb6ea18e84fe3c57804
anksingh90 <ankitsingh90@duck.com>
2025-01-31 11:34:14 +0000

    ch

--- file_handle/f5.py
+++ file_handle/f5.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 file_path = os.path.join("data", "ex.txt")      # Joins path components correctly for different operating systems.
 absolute_path = os.path.abspath(file_path)      # Gets the absolute path
