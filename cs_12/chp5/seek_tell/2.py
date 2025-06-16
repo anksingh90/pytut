89dbeaa4583a765d22e0a83fa52b20d3f75ec040
anksingh90 <ankitsingh90@duck.com>
2025-06-16 08:40:14 +0000

    update

---
+++ cs_12/chp5/seek_tell/2.py
@@ -0,0 +1,5 @@
+'''
+Question 2: Read and Rewind
+Problem: Write a script that reads the first line of story.txt, prints it, and then reads and prints 
+the entire file from the beginning without closing and reopening it.
+'''
---
+++ cs_12/chp5/seek_tell/3.py
@@ -0,0 +1,4 @@
+'''
+Question 2: What's My Position?
+Problem: Read story.txt line by line. After reading each line, print the line itself and the cursor's position number returned by tell().
+'''
