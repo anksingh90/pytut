50443a12669fff9406f42dca3d4b8d68026ad0de
anksingh90 <ankitsingh90@duck.com>
2025-06-25 07:18:02 +0000

    update

--- cs_12/chp5/txt/old_files/11.py
+++ cs_12/chp5/txt/old_files/11.py
@@ -1,8 +1,7 @@
-object =open('data.txt', 'r')
-print(object)
-print("Name of File:",object.name)
-print("Mode of File:",object.mode)
-print("File readable :",object.readable())
-print("File closed:",object.closed)
-object.close()
-print("File closed:",object.closed, object.name)
+import csv
+
+my_connection = open("data.csv","r",newline='\r\n')
+obj = csv.reader(my_connection)
+
+for i in obj:
+    print(i)
